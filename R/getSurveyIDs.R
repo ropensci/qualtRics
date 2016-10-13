d9a08a91a927eae92d136df9c10eb90b320427b9
Jasper Ginn <jasperginn@hotmail.com>
2016-10-13 15:18:37 +0200

    Update params

--- R/getSurveyIDs.R
+++ R/getSurveyIDs.R
@@ -1,12 +1,13 @@
 #' Retrieve a list of all active surveys that you own on qualtrics
 #'
-#' @param
+#' @param headers 'headers' object - returned by the 'constructHeader' function. See \link[qualtRics]{constructHeader}.
+#' @param survey_baseurl Base url for your institution (see \url{https://api.qualtrics.com/docs/root-url}. If you do not fill in anything, the function will use the default url. Using your institution-specific url can significantly speed up queries.)
 #'
-#' @seealso See \url{https://api.qualtrics.com/docs/list-surveys} for documentation on the Qualtrics API.
+#' @seealso See \url{https://api.qualtrics.com/docs} for documentation on the Qualtrics API.
 #' @author Jasper Ginn
 #' @importFrom httr GET
 
-getSurveyIDs <- function(survey_baseurl = "https://yourdatacenterid.qualtrics.com/API/v3/surveys", headers) {
+getSurveyIDs <- function(headers, survey_baseurl = "https://yourdatacenterid.qualtrics.com/API/v3/surveys") {
   # Send GET request to list all surveys
   res <- GET(survey_baseurl, add_headers(headers))
   # Return
