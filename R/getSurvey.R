be498c7c50fe4f3c8ffa64ace2077ba0b0d1d00d
Jasper Ginn <jasperginn@hotmail.com>
2016-11-07 10:26:00 +0100

    Updated documentation. No longer allowed to download surveys without supplying institution base url.

--- R/getSurvey.R
+++ R/getSurvey.R
@@ -20,7 +20,7 @@
 #'
 #' @param surveyID Unique ID for the survey you want to download. Returned as 'id' by the \link[qualtRics]{getSurveys} function.
 #' @param headers 'headers' object - returned by the 'constructHeader' function. See \link[qualtRics]{constructHeader}.
-#' @param base_url Base url for your institution (see \url{https://api.qualtrics.com/docs/csv}. If you do not fill in anything, the function will use the default url. Using your institution-specific url can significantly speed up queries.)
+#' @param base_url Base url for your institution (see \url{https://api.qualtrics.com/docs/csv}. You need to supply this url. Your query will NOT work without it.)
 #' @param verbose Print verbose messages to the R console? Defaults to FALSE
 #'
 #' @seealso See \url{https://api.qualtrics.com/docs/csv} for documentation on the Qualtrics API.
