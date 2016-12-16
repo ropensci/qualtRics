e586862599f77d51e31fee59794fcd6b74ad850e
Jasper Ginn <jasperginn@hotmail.com>
2016-12-16 18:01:26 +0100

    Forgot 'expect_error'

--- tests/testthat/test-getSurvey_throws_error.R
+++ tests/testthat/test-getSurvey_throws_error.R
@@ -2,5 +2,6 @@ test_that("getSurvey() throws error", {
   # Register dud API key
   qualtRics::registerApiKey("ABCD")
   # Query fake ID with generic root url
-  qualtRics::getSurvey("1234", "https://yourdatacenterid.qualtrics.com")
+  expect_error(qualtRics::getSurvey("1234", "https://yourdatacenterid.qualtrics.com"),
+               'Query returned status "401 - Unauthorized". Please check your code.')
 })
