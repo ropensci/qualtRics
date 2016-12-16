3beb120b3de6276e2e74130fc8361c532d2e0e0e
Jasper Ginn <jasperginn@hotmail.com>
2016-12-16 17:23:17 +0100

    Removed is.logical

--- R/assertions.R
+++ R/assertions.R
@@ -3,7 +3,7 @@
 # Test whether API key is stored
 assert_apikey_stored <- function(dir) {
   # Key should be stored by "registerApiKey"
-  if(!assertthat::assert_that(assertthat::is.logical("qualtRics_header.rds" %in% list.files(dir)))) {
+  if(!assertthat::assert_that("qualtRics_header.rds" %in% list.files(dir))) {
     stop("You need to register your qualtrics API key first using the 'registerApiKey()' function.")
   } else {
     return(TRUE)
