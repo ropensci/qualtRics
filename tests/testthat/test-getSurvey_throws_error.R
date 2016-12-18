d0cf5f0af2562bd2a259418fe0e6eddea0600c5a
Jasper Ginn <jasperginn@hotmail.com>
2016-12-18 10:33:06 +0100

    Update testthat errors

--- tests/testthat/test-getSurvey_throws_error.R
+++ tests/testthat/test-getSurvey_throws_error.R
@@ -3,5 +3,5 @@ test_that("getSurvey() throws error", {
   qualtRics::registerApiKey("ABCD")
   # Query fake ID with generic root url
   expect_error(qualtRics::getSurvey("1234", "https://yourdatacenterid.qualtrics.com"),
-               'Qualtrics API raised an authentication (401) error - you may not have the required authorization. Please check your API key and root url.')
+               'Qualtrics API raised an authentication (401) error')
 })
--- tests/testthat/test-getSurveys_throws_error.R
+++ tests/testthat/test-getSurveys_throws_error.R
@@ -2,5 +2,5 @@ test_that("getSurvey() throws an error", {
   # Register dud API key
   qualtRics::registerApiKey("ABCD")
   # This should fail in 'do.call'
-  expect_error(qualtRics::getSurveys(), "Qualtrics API raised an authentication (401) error - you may not have the required authorization. Please check your API key and root url.")
+  expect_error(qualtRics::getSurveys(), "Qualtrics API raised an authentication (401) error")
 })
