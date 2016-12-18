8fe9378855b39a3674784770adc7dda78ca7e71b
Jasper Ginn <jasperginn@hotmail.com>
2016-12-18 10:46:38 +0100

    Testthat cannot handle parentheses ...

--- tests/testthat/test-getSurveys_throws_error.R
+++ tests/testthat/test-getSurveys_throws_error.R
@@ -1,6 +1,7 @@
-test_that("getSurvey() throws an error", {
+test_that("getSurveys() throws an error", {
   # Register dud API key
   qualtRics::registerApiKey("ABCD")
   # This should fail in 'do.call'
-  expect_error(qualtRics::getSurveys(), "Qualtrics API raised an authentication (401) error")
+  expect_error(qualtRics::getSurveys(),
+               "you may not have the required authorization. Please check your API key and root url.")
 })
