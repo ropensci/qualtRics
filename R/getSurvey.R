092653c2122b0d711e000bbf3e02a97a49132167
Jasper Ginn <jasperginn@hotmail.com>
2017-05-23 07:59:09 +0200

    Add limit and uselocaltime parameters and rewrote parameters for getSurvey

--- R/getSurvey.R
+++ R/getSurvey.R
@@ -18,17 +18,19 @@
 #'
 #' Export a qualtrics survey you own and import the survey directly into R. NOTE: If you keep getting errors try to use your institution's base URL. See \url{https://api.qualtrics.com/docs/root-url}.
 #'
-#' @param surveyID Unique ID for the survey you want to download. Returned as 'id' by the \link[qualtRics]{getSurveys} function.
-#' @param root_url Base url for your institution (see \url{https://api.qualtrics.com/docs/csv}. You need to supply this url. Your query will NOT work without it.).
-#' @param useLabels TRUE to export survey responses as Choice Text or FALSE to export survey responses as values
-#' @param convertStandardColumns logical, defaults to TRUE. If TRUE, then the function will convert general data columns (first name, last name, lat, lon, ip address, startdate, enddate etc.) to their proper format.
-#' @param lastResponseId Export all responses received after the specified response
-#' @param startDate Date range filter to only exports responses recorded after the specified date. Accepts dates as character strings in format "YYYY-MM-DD"
-#' @param endDate Date range filter to only exports responses recorded before the specified date. Accepts dates as character strings in format "YYYY-MM-DD"
-#' @param seenUnansweredRecode Recode seen but unanswered questions with a string value. Defaults to NULL.
-#' @param save_dir Directory where survey results will be stored. Defaults to a temporary directory which is cleaned when your R session is terminated. This parameter is useful if you'd like to store survey results. The downloaded survey will be stored as an RDS file (see \link[base]{readRDS}).
-#' @param force_request getSurvey() saves each survey in a temporary directory so that it can quickly be retrieved later. If force_request is TRUE, getSurvey() always downloads the survey from the API instead of loading it from the temporary directory.
-#' @param verbose Print verbose messages to the R console? Defaults to FALSE
+#' @param surveyID String. Unique ID for the survey you want to download. Returned as 'id' by the \link[qualtRics]{getSurveys} function.
+#' @param root_url String. Base url for your institution (see \url{https://api.qualtrics.com/docs/csv}. You need to supply this url. Your query will NOT work without it.).
+#' @param useLabels Boolean. TRUE to export survey responses as Choice Text or FALSE to export survey responses as values.
+#' @param convertStandardColumns Boolean. If TRUE, then the function will convert general data columns (first name, last name, lat, lon, ip address, startdate, enddate etc.) to their proper format. Defaults to TRUE
+#' @param lastResponseId String. Export all responses received after the specified response. Defaults to NULL.
+#' @param startDate Date. Filter to only exports responses recorded after the specified date. Accepts dates as character strings in format "YYYY-MM-DD". Defaults to NULL.
+#' @param endDate Date. Filter to only exports responses recorded before the specified date. Accepts dates as character strings in format "YYYY-MM-DD". Defaults to NULL.
+#' @param seenUnansweredRecode String. Recode seen but unanswered questions with a string value. Defaults to NULL.
+#' @param limit Integer. Maximum number of responses exported. Defaults to NULL (all responses).
+#' @param useLocalTime Boolean. Use local timezone to determine response date values. Defaults to FALSE.
+#' @param save_dir String. Directory where survey results will be stored. Defaults to a temporary directory which is cleaned when your R session is terminated. This parameter is useful if you'd like to store survey results. The downloaded survey will be stored as an RDS file (see \link[base]{readRDS}).
+#' @param force_request Boolean. getSurvey() saves each survey in a temporary directory so that it can quickly be retrieved later. If force_request is TRUE, getSurvey() always downloads the survey from the API instead of loading it from the temporary directory. Defaults to FALSE.
+#' @param verbose Print verbose messages to the R console? Defaults to FALSE.
 #'
 #' @seealso See \url{https://api.qualtrics.com/docs/csv} for documentation on the Qualtrics API.
 #' @author Jasper Ginn
@@ -60,6 +62,8 @@ getSurvey <- function(surveyID,
                       startDate=NULL,
                       endDate=NULL,
                       seenUnansweredRecode=NULL,
+                      limit = NULL,
+                      useLocalTime = FALSE,
                       save_dir=NULL,
                       force_request=FALSE,
                       verbose=FALSE) {
