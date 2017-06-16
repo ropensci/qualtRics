68008d9a5970f6913ee8fb365ff2ebd6c2458f22
Jasper Ginn <jasperginn@hotmail.com>
2017-06-16 14:50:03 +0200

    Set API key to "" before test

--- tests/testthat/test-assert_register_api_key.R
+++ tests/testthat/test-assert_register_api_key.R
@@ -1,7 +1,7 @@
 test_that("RegisterApiKey returns TRUE", {
   # This should fail because no key was stored
+  Sys.setenv("QUALTRICS_API_KEY"="")
   expect_error(assert_apikey_stored(), "You need to register your qualtrics API key")
-  cat(Sys.getenv("QUALTRICS_API_KEY"))
   # Store dummy key
   qualtRics::registerOptions(api_token="1234", root_url="abcd")
   # Now expect this to be true
