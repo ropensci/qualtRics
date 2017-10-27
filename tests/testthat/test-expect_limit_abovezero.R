986507ff991e368d74aba1d983d9fe6a2464abfb
Jasper Ginn <jasperginn@hotmail.com>
2017-10-27 11:41:51 +0200

    Updated mock root url in test

--- tests/testthat/test-expect_limit_abovezero.R
+++ tests/testthat/test-expect_limit_abovezero.R
@@ -1,6 +1,6 @@
 test_that("Limit cannot be less than one", {
   # Store dummy key
-  qualtRics::registerOptions(api_token="1234", root_url="abcd")
+  qualtRics::registerOptions(api_token="1234", root_url="abcd.qualtrics.com")
   # Call getsurvey
   expect_error(qualtRics::getSurvey("1234", limit = 0),
                "Limit parameter should be at least 1")
