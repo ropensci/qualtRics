8f6f21d4754cdd0d7e22ead0d1a5a287f0cc3747
Jasper Ginn <jasperginn@hotmail.com>
2018-02-05 08:31:52 +0100

    Use on.exit() to restore wd

--- tests/testthat/test-getSurvey_reads_stored_survey.R
+++ tests/testthat/test-getSurvey_reads_stored_survey.R
@@ -4,9 +4,12 @@ test_that("getSurvey() reads a stored survey in temporary directory if exists", 
                              root_url="https://yourdatacenterid.qualtrics.com")
   # Store RDS file
   data <- "SUCCESS"
+  curr.wd <- getwd()
   setwd(tempdir())
+  on.exit(setwd(curr.wd))
   saveRDS(data, "surveyID.rds")
   # Query with all options
   expect_message(qualtRics::getSurvey("surveyID"),
                  "Loading this file. Set 'force_request' to TRUE if you want to override this.")
+
 })
