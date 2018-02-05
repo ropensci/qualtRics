b13c30ed2aa8c282551fd0918feeadcdfc82a4b4
Jasper Ginn <jasperginn@hotmail.com>
2018-02-05 09:06:22 +0100

    seq_along --> 1:nrow(df)

--- R/getSurveyQuestions.R
+++ R/getSurveyQuestions.R
@@ -58,7 +58,7 @@ getSurveyQuestions <- function(surveyID) {
                       stringsAsFactors = FALSE)
 
   # Row names
-  row.names(quest) <- 1:nrow(quest)
+  row.names(quest) <- seq_along(nrow(s))
   # Return
   return(dplyr::as_tibble(quest))
 }
