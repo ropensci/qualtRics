efd4ac5292f0de179a55033c085695dbd451a226
Jasper Ginn <jasperginn@hotmail.com>
2018-02-05 09:22:43 +0100

    typo

--- R/getSurveyQuestions.R
+++ R/getSurveyQuestions.R
@@ -58,7 +58,7 @@ getSurveyQuestions <- function(surveyID) {
                       stringsAsFactors = FALSE)
 
   # Row names
-  row.names(quest) <- seq_along(nrow(quest))
+  row.names(quest) <- seq_along(1:nrow(quest))
   # Return
   return(dplyr::as_tibble(quest))
 }
