b5edacd88117fb68301ac4e94314617ee48641e9
Jasper Ginn <jasperginn@hotmail.com>
2018-03-27 12:40:18 +0200

    Update test

--- tests/testthat/test-checkParams_checks_work.R
+++ tests/testthat/test-checkParams_checks_work.R
@@ -2,7 +2,7 @@ test_that("Arguments are properly checked by checkParams()", {
   # This should fail because we can't connect to the qualtRics API
   # Store dummy key
   qualtRics::registerOptions(api_token="1234",
-                             root_url="https://yourdatacenterid.qualtrics.com")
+                             base_url="https://yourdatacenterid.qualtrics.com")
   # Query with all options
   expect_error(qualtRics::getSurvey("1234",
                                     lastResponseId = "1234",
@@ -10,6 +10,6 @@ test_that("Arguments are properly checked by checkParams()", {
                                     endDate="2017-01-31",
                                     seenUnansweredRecode = "UA",
                                     includedQuestionIds = c("QID1", "QID2"),
-                                    save_dir=tempdir()),
-               "you may not have the required authorization")
+                                    saveDir=tempdir()),
+               "you may not have the\nrequired authorization")
 })
