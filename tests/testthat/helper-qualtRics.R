812d4b8d1538ad864c005c92f210911e9df7d52d
Daniel Seneca <seneca.danielh@gmail.com>
2020-05-01 11:22:21 -0700

    add filter_sensitive_data to vcr_configure

--- tests/testthat/helper-qualtRics.R
+++ tests/testthat/helper-qualtRics.R
@@ -2,5 +2,7 @@
 library("vcr")
 invisible(vcr::vcr_configure(
   dir = "tests/fixtures",
-  preserve_exact_body_bytes = TRUE
+  preserve_exact_body_bytes = TRUE,
+  filter_sensitive_data = list("<<<my_api_key>>>" = Sys.getenv('QUALTRICS_API_KEY'),
+                               "<<<my_base_url>>>" = Sys.getenv('QUALTRICS_BASE_URL'))
 ))
