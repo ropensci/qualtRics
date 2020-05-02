9db7de42fdcfb0e57a32c43d39d676ed1a4bd9b5
Julia Silge <julia.silge@gmail.com>
2020-05-02 16:56:12 -0600

    Write fixtures to tests/fixtures/

--- tests/testthat/helper-qualtRics.R
+++ tests/testthat/helper-qualtRics.R
@@ -1,7 +1,7 @@
 
 library("vcr")
 invisible(vcr::vcr_configure(
-  dir = "tests/fixtures",
+  dir = "../fixtures",
   preserve_exact_body_bytes = TRUE,
   filter_sensitive_data = list("<<<my_api_key>>>" = Sys.getenv('QUALTRICS_API_KEY'),
                                "<<<my_base_url>>>" = Sys.getenv('QUALTRICS_BASE_URL'))
