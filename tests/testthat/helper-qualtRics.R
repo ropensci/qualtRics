9db3ff36ba8883333775d845ae20d3fc2e36b748
Joseph O'Brien <joseph.m.obrien@gmail.com>
2020-12-14 10:10:05 -0600

    Change to make VCR write fixtures in expected format for subsequent tests.

--- tests/testthat/helper-qualtRics.R
+++ tests/testthat/helper-qualtRics.R
@@ -4,5 +4,5 @@ invisible(vcr::vcr_configure(
   dir = "../fixtures",
   preserve_exact_body_bytes = TRUE,
   filter_sensitive_data = list("<<<my_api_key>>>" = Sys.getenv('QUALTRICS_API_KEY'),
-                               "<<<my_base_url>>>" = Sys.getenv('QUALTRICS_BASE_URL'))
+                               "t.qualtrics.com" = Sys.getenv('QUALTRICS_BASE_URL'))
 ))
