bc5954192fca94486aff8f6ac88da4dc358f4a40
Julia Silge <julia.silge@gmail.com>
2025-08-04 20:04:27 -0600

    Use withr for env vars

--- tests/testthat/test-generate-url.R
+++ tests/testthat/test-generate-url.R
@@ -1,32 +1,26 @@
 test_that("all surveys URL", {
-  Sys.setenv("QUALTRICS_BASE_URL" = "www.qualtrics.com")
-  expect_equal(
-    generate_url("allsurveys"),
-    "https://www.qualtrics.com/API/v3/surveys"
-  )
-})
-
-
-test_that("fetch surveys URL", {
-  Sys.setenv("QUALTRICS_BASE_URL" = "www.qualtrics.com")
-  expect_equal(
-    generate_url("exportresponses", surveyID = "abcdefg"),
-    "https://www.qualtrics.com/API/v3/surveys/abcdefg/export-responses/"
-  )
-})
-
-test_that("fetch distributions URL", {
-  Sys.setenv("QUALTRICS_BASE_URL" = "www.qualtrics.com")
-  expect_equal(
-    generate_url("fetchdistributions", surveyID = "potato"),
-    "https://www.qualtrics.com/API/v3/distributions?surveyId=potato"
-  )
-})
-
-test_that("error on non-existent URL", {
-  Sys.setenv("QUALTRICS_BASE_URL" = "www.qualtrics.com")
-  expect_error(
-    generate_url("potatoURL", surveyID = "potato"),
-    "Internal error: invalid URL generation query"
+  withr::with_envvar(
+    new = c(
+      "QUALTRICS_API_KEY" = "1234",
+      "QUALTRICS_BASE_URL" = "www.qualtrics.com"
+    ),
+    {
+      expect_equal(
+        generate_url("allsurveys"),
+        "https://www.qualtrics.com/API/v3/surveys"
+      )
+      expect_equal(
+        generate_url("exportresponses", surveyID = "abcdefg"),
+        "https://www.qualtrics.com/API/v3/surveys/abcdefg/export-responses/"
+      )
+      expect_equal(
+        generate_url("fetchdistributions", surveyID = "potato"),
+        "https://www.qualtrics.com/API/v3/distributions?surveyId=potato"
+      )
+      expect_error(
+        generate_url("potatoURL", surveyID = "potato"),
+        "Internal error: invalid URL generation query"
+      )
+    }
   )
 })
--- tests/testthat/test-qualtrics-api-request.R
+++ tests/testthat/test-qualtrics-api-request.R
@@ -3,43 +3,45 @@ library(webmockr)
 test_that("it should throw an error after certain 400 and 500 status codes", {
   skip_on_cran()
 
-  webmockr::enable()
-  mock_base_url <- 'www.qualtrics.com'
-  mock_url <- 'https://testUrl.com'
-  verb <- 'GET'
-  mock_api_key <- '1234'
-  qualtrics_api_credentials(mock_api_key, mock_base_url)
-  headers <- list(
-    'X-API-TOKEN' = mock_api_key,
-    'Content-Type' = 'application/json',
-    'Accept' = '*/*',
-    'accept-encoding' = 'gzip, deflate'
+  withr::with_envvar(
+    new = c(
+      "QUALTRICS_API_KEY" = "1234",
+      "QUALTRICS_BASE_URL" = "www.qualtrics.com"
+    ),
+    {
+      webmockr::enable()
+      mock_url <- 'https://testUrl.com'
+      verb <- 'GET'
+      headers <- list(
+        'X-API-TOKEN' = Sys.getenv("QUALTRICS_API_KEY"),
+        'Content-Type' = 'application/json',
+        'Accept' = '*/*',
+        'accept-encoding' = 'gzip, deflate'
+      )
+      stub_req <- stub_request(verb, mock_url) %>% wi_th(headers = headers)
+
+      stub_req %>% to_return(status = 401)
+      expect_error(qualtrics_api_request(verb, mock_url))
+
+      stub_req %>% to_return(status = 404)
+      expect_error(qualtrics_api_request(verb, mock_url))
+
+      stub_req %>% to_return(status = 403)
+      expect_error(qualtrics_api_request(verb, mock_url))
+
+      stub_req %>% to_return(status = 500)
+      expect_error(qualtrics_api_request(verb, mock_url))
+
+      stub_req %>% to_return(status = 503)
+      expect_error(qualtrics_api_request(verb, mock_url))
+
+      stub_req %>% to_return(status = 413)
+      expect_error(qualtrics_api_request(verb, mock_url))
+
+      stub_req %>% to_return(status = 429)
+      expect_error(qualtrics_api_request(verb, mock_url))
+
+      webmockr::disable()
+    }
   )
-  stub_req <- stub_request(verb, mock_url) %>% wi_th(headers = headers)
-
-  stub_req %>% to_return(status = 401)
-  expect_error(qualtrics_api_request(verb, mock_url))
-
-  stub_req %>% to_return(status = 404)
-  expect_error(qualtrics_api_request(verb, mock_url))
-
-  stub_req %>% to_return(status = 403)
-  expect_error(qualtrics_api_request(verb, mock_url))
-
-  stub_req %>% to_return(status = 500)
-  expect_error(qualtrics_api_request(verb, mock_url))
-
-  stub_req %>% to_return(status = 503)
-  expect_error(qualtrics_api_request(verb, mock_url))
-
-  stub_req %>% to_return(status = 413)
-  expect_error(qualtrics_api_request(verb, mock_url))
-
-  stub_req %>% to_return(status = 429)
-  expect_error(qualtrics_api_request(verb, mock_url))
-
-  webmockr::disable()
 })
-
-# Restore the credentials for other tests:
-qualtrics_api_credentials(api_key = holder_API, base_url = holder_URL)
